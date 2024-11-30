4d416b0780be4f88a4ae3850347b13a2e816c415
jawad093 <j.ali861877@gmail.com>
2024-11-30 19:16:32 +0500

    Update task5.py

--- Artifical_intelligence/Section 3B/task5.py
+++ Artifical_intelligence/Section 3B/task5.py
@@ -1,45 +1,42 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.children = []  
+        self.children = [] 
 
     def add_child(self, child_node):
         self.children.append(child_node)  
 
-
-def dfs(start_node, goal):
-    stack = [start_node]  
+def DFS(initial_node, target_value):
+    stack = [initial_node]  
     visited = []  
 
     while stack:
-        current_node = stack.pop()
-        
+        current_node = stack.pop()  
+
         if current_node.data not in visited:
-            visited.append(current_node.data) 
-            
+            visited.append(current_node.data)  
             print(current_node.data, end=" ")  
-            
-            if current_node.data == goal:  
+
+            if current_node.data == target_value:  
                 return
 
-      
-            for child in current_node.children:
+            for child in current_node.children:  
                 stack.append(child)
 
 
-Starting_root = Node("a")
-b = Node("b")
-c = Node("c")
-d = Node("d")
-e = Node("e")
-f = Node("f")
+Starting_root = Node("LAHore")
+faslabad = Node("faslabad")
+karachi = Node("karachi")
+multan = Node("multan")
+chock = Node("chock")
+feroz = Node("feroz")
 
 
-Starting_root.add_child(b)
-Starting_root.add_child(c)
-b.add_child(d)
+Starting_root.add_child(faslabad)
+Starting_root.add_child(karachi)
+faslabad.add_child(multan) 
+karachi.add_child(chock)    
+chock.add_child(feroz)      
 
-c.add_child(e)
-c.add_child(f) 
 
-dfs(Starting_root, 'f')
+DFS(Starting_root, 'feroz')
